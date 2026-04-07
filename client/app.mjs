b382fe37916a108db2ebea8346119efb54ea8325
macbook <macbook@macbookair.lan>
2026-04-07 13:38:03 +0200

    made log out button clear local storage

--- client/app.mjs
+++ client/app.mjs
@@ -6,3 +6,19 @@ export async function apiFetch(url, options = {}) {
 
 	return response.json();
 }
+
+//----------------------------------------------------------------------------
+
+document.addEventListener("DOMContentLoaded", () => {
+	const logoutButtons = document.querySelectorAll(".logout");
+
+	logoutButtons.forEach((btn) => {
+		btn.addEventListener("click", () => {
+			console.log("Logout clicked"); // 👈 debug
+
+			localStorage.removeItem("userId");
+
+			window.location.href = "/client/views/frontPage.html";
+		});
+	});
+});
