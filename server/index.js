e3758a3b2072c92a0e87e696df2577010b97b389
macbook <macbook@macbookair.lan>
2026-04-07 20:25:39 +0200

    add clean routing for html pages

--- server/index.js
+++ server/index.js
@@ -13,16 +13,63 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// 👇 SERVE FRONTEND
+// SERVE FRONTEND
 app.use(express.static(path.join(__dirname, "../client")));
 
-// 👇 ROUTES
+// ROUTES
 app.use("/books", bookRoutes);
 app.use("/users", userRoutes);
 
-// 👇 LOAD FRONT PAGE
+// LOAD FRONT PAGE
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "../client/views/frontPage.html"));
+});
+
+const path = require("path");
+
+// FRONT PAGE
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "../client/views/frontPage.html"));
+});
+
+// CLEAN ROUTES
+app.get("/loginPage.html", (req, res) => {
+	res.sendFile(path.join(__dirname, "../client/views/loginPage.html"));
+});
+
+app.get("/createUser.html", (req, res) => {
+	res.sendFile(path.join(__dirname, "../client/views/createUser.html"));
+});
+
+app.get("/homePage.html", (req, res) => {
+	res.sendFile(path.join(__dirname, "../client/views/homePage.html"));
+});
+
+app.get("/userPage.html", (req, res) => {
+	res.sendFile(path.join(__dirname, "../client/views/userPage.html"));
+});
+
+app.get("/addBooks.html", (req, res) => {
+	res.sendFile(path.join(__dirname, "../client/views/addBooks.html"));
+});
+
+// EDIT USER ROUTES
+app.get("/editUser/editUserName.html", (req, res) => {
+	res.sendFile(
+		path.join(__dirname, "../client/views/editUser/editUserName.html")
+	);
+});
+
+app.get("/editUser/editPassword.html", (req, res) => {
+	res.sendFile(
+		path.join(__dirname, "../client/views/editUser/editPassword.html")
+	);
+});
+
+app.get("/editUser/deleteAccount.html", (req, res) => {
+	res.sendFile(
+		path.join(__dirname, "../client/views/editUser/deleteAccount.html")
+	);
 });
 
 async function createTable() {
